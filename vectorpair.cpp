2a37b040f40867babd9b77ec522f2e166fa26873
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-02-26 15:21:14 +0530

    Update vectorpair.cpp

--- vectorpair.cpp
+++ vectorpair.cpp
@@ -31,16 +31,60 @@ int main()
     //         cout<<g[i].first<<" "<<g[i].second<<"\n";
     //     }
 
-    vector<pair<int, pair<int, int>>> myvec;
-       myvec.push_back(make_pair(2, make_pair(31, 102))); 
-          myvec.push_back(make_pair(2, make_pair(31, 102))); 
-             myvec.push_back(make_pair(2, make_pair(31, 102))); 
-             int s=myvec.size();
+//     vector<pair<int, pair<int, int>>> myvec;
+//        myvec.push_back(make_pair(2, make_pair(31, 102))); 
+//           myvec.push_back(make_pair(2, make_pair(31, 102))); 
+//              myvec.push_back(make_pair(2, make_pair(31, 102))); 
+//              int s=myvec.size();
 
-             for(int i=0;i<s;i++){
-                 cout<<myvec[i].first<<","<<myvec[i].second.first<<","<<myvec[i].second.second<<"\n";
-             }
+//              for(int i=0;i<s;i++){
+//                  cout<<myvec[i].first<<","<<myvec[i].second.first<<","<<myvec[i].second.second<<"\n";
+//              }
 
-             
+    
+    
+         //four parameter vector pair
+               vector<pair< pair<int ,int>,pair<string,int>>> myvec;
+     myvec.push_back(make_pair(make_pair(0,1), make_pair("abc",1)));
+         myvec.push_back(make_pair(make_pair(2,1), make_pair("bbc",2)));
+             myvec.push_back(make_pair(make_pair(3,1), make_pair("adc",3)));
+                 myvec.push_back(make_pair(make_pair(4,6), make_pair("abd",5)));
+    //   myvec.push_back(make_pair(3,make_pair(1,1)));
+    //    myvec.push_back(make_pair(2,make_pair(3,2)));
+    //    myvec.push_back(make_pair(1,make_pair(2,3)));
+
+
+    for(int i=0;i<myvec.size();i++){
+
+      cout<<myvec[i].first.first<<" "<<myvec[i].first.second<<" "<<myvec[i].second.first<<" "<<myvec[i].second.second<<" \n";
+    }
+    
+
+      pair<pair<int,int>,pair<string,int>> p[5];
+
+
+      for(int i=0;i<5;i++){
+
+            int a,b,c;
+            string str;
+            cin>>a>>b;
+            cin>>str;
+            cin>>c;
+
+
+            p[i].first.first=a;
+            p[i].first.second=b;
+            p[i].second.first=str;
+            p[i].second.second=c;
+        
+      }
+
+
+      for(int i=0;i<5;i++){
+     cout<<p[i].first.first<<" "<< p[i].first.second<< " "<<p[i].second.first<<" "<<p[i].second.second<<"\n";
+
+      }
+     
+
 
 }

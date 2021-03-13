0ff34ca876f5075d4928b3a48b1a7e0336b913f3
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-03-13 23:53:53 +0530

    Add files via upload

---
+++ string-impResult.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // cout<<char('a')<< "\n";
+    string temp= (char)(2+'c')+"\0 ";
+    cout<<"this is\t"<<temp<<" \n";
+    // for(int i=0;i<26;i++){
+    //     cout<<'a'+i<<" ";
+    // }
+}
+
